bef3038123c1cf56dbe7d276f240abc2d41bf1b1
Valentin <115096856+ME0NCONILEX@users.noreply.github.com>
2022-12-07 15:07:34 +0100

    Update AppTest.java

--- src/test/java/se/me0nly/AppTest.java
+++ src/test/java/se/me0nly/AppTest.java
@@ -4,9 +4,7 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
+/*  Unit test for simple App. */
 public class AppTest 
     extends TestCase
 {
@@ -20,19 +18,14 @@ public class AppTest
         super( testName );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
+    /*  @return the suite of tests being tested */
     public static Test suite()
     {
         return new TestSuite( AppTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
+    /* Rigourous Test :-) */
     public void testApp()
     {
         assertTrue( true );
     }
-}
